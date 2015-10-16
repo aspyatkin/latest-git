9b7a98bd35d5e5cd5bde6b2d9f2aee6cdea248f4
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2015-10-16 14:50:01 +0400

    Wrap git cookbook

---
+++ README.md
@@ -0,0 +1,2 @@
+#indigobyte-git
+Install git 2.6 from source.
---
+++ metadata.rb
@@ -0,0 +1,6 @@
+name 'latest-git'
+description 'Installs and configures git'
+version '1.0.0'
+
+recipe 'latest-git', 'Installs and configures git'
+depends 'git', '4.3.4'
