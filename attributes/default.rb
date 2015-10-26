4bbb80f7521a79b22090e23a4bd95d35f0317ae6
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2015-10-26 11:04:12 +0400

    Add editorconfig, Berksfile, LICENSE

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,5 @@
-override['git']['version'] = '2.6.0'
-override['git']['checksum'] = '807ebf4ea8a96a9a7e48be5101fb23e31135c1a084b041a25f9a5a76297d8140'
+override['git']['version'] = '2.6.2'
+override['git']['url'] = "https://www.kernel.org/pub/software/scm/git/git-#{override['git']['version']}.tar.gz"
+override['git']['checksum'] = '34dfc06b44880df91940dc318a2d3c83b79e67b6f05319c7c71e94d30893636d'
 
 node.from_file(run_context.resolve_attribute('git', 'default'))
