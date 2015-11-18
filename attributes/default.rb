66fa0ded50bc53adf027abba81c7a876fdc5ee72
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2015-11-18 10:16:00 +0400

    Update git version

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
-override['git']['version'] = '2.6.2'
+override['git']['version'] = '2.6.3'
 override['git']['url'] = "https://www.kernel.org/pub/software/scm/git/git-#{override['git']['version']}.tar.gz"
-override['git']['checksum'] = '34dfc06b44880df91940dc318a2d3c83b79e67b6f05319c7c71e94d30893636d'
+override['git']['checksum'] = '9c892df67fec64f1974e960491734ec69ffc9bef670645148ab339b62b688d91'
 
 node.from_file(run_context.resolve_attribute('git', 'default'))
