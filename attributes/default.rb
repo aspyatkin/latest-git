7ff395f12e5ee76bd2fd6748386ff5514b4da016
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2015-12-03 16:12:21 +0400

    Don't use override

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,4 @@
-override['git']['version'] = '2.6.3'
-override['git']['url'] = "https://www.kernel.org/pub/software/scm/git/git-#{override['git']['version']}.tar.gz"
-override['git']['checksum'] = '9c892df67fec64f1974e960491734ec69ffc9bef670645148ab339b62b688d91'
-
-node.from_file(run_context.resolve_attribute('git', 'default'))
+default['git']['version'] = '2.6.3'
+default['git']['url'] = "https://www.kernel.org/pub/software/scm/git/git-#{default['git']['version']}.tar.gz"
+default['git']['checksum'] = '9c892df67fec64f1974e960491734ec69ffc9bef670645148ab339b62b688d91'
+default['git']['use_pcre'] = true
--- metadata.rb
+++ metadata.rb
@@ -2,14 +2,14 @@ name 'latest-git'
 maintainer 'Alexander Pyatkin'
 maintainer_email 'aspyatkin@gmail.com'
 license 'MIT'
-version '1.1.1'
+version '1.1.2-dev.0'
 description 'Installs and configures git'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 
 recipe 'latest-git', 'Installs and configures git'
 depends 'git', '~> 4.3.4'
 
-source_url 'https://github.com/aspyatkin/latest-git' if respond_to?(:source_url)
+source_url 'https://github.com/aspyatkin/latest-git' if respond_to? :source_url
 
 supports 'ubuntu'
 supports 'centos'
