48f818fe0db30b550641dbab6a1a1d5887cf4e03
Alexander Pyatkin <(none)>
2016-03-29 19:14:37 +0400

    Update git version

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default['git']['version'] = '2.7.3'
+default['git']['version'] = '2.8.0'
 default['git']['url'] = "https://www.kernel.org/pub/software/scm/git/git-#{default['git']['version']}.tar.gz"
-default['git']['checksum'] = '30d067499b61caddedaf1a407b4947244f14d10842d100f7c7c6ea1c288280cd'
+default['git']['checksum'] = '2c6eee5506237e0886df9973fd7938a1b2611ec93d07f64ed3447493ebac90d1'
 default['git']['use_pcre'] = true
--- metadata.rb
+++ metadata.rb
@@ -2,12 +2,12 @@ name 'latest-git'
 maintainer 'Alexander Pyatkin'
 maintainer_email 'aspyatkin@gmail.com'
 license 'MIT'
-version '1.1.5'
+version '1.1.6'
 description 'Installs and configures git'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 
 recipe 'latest-git', 'Installs and configures git'
-depends 'git', '~> 4.3.7'
+depends 'git', '~> 4.4.0'
 depends 'build-essential', '~> 2.2.4'
 
 source_url 'https://github.com/aspyatkin/latest-git' if respond_to? :source_url
