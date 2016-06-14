bfeaff2efbfa64cefb399af32f2693fdd5cf7215
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2016-06-14 15:05:57 +0400

    Update git to 2.9.0

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 id = 'latest-git'
 
-default[id]['version'] = '2.8.3'
+default[id]['version'] = '2.9.0'
 default[id]['url_template'] = 'https://www.kernel.org/pub/software/scm/git/git-%{version}.tar.gz'
-default[id]['checksum'] = '2dad50c758339d6f5235309db620e51249e0000ff34aa2f2acbcb84c2123ed09'
+default[id]['checksum'] = 'bff7560f5602fcd8e37669e0f65ef08c6edc996e4f324e4ed6bb8a84765e30bd'
