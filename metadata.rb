7dc5e8d0a182d55fcdaddf1e28da01efbb1cbc1e
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2017-09-04 20:43:18 +0400

    Update git cookbook version

--- metadata.rb
+++ metadata.rb
@@ -2,12 +2,12 @@ name 'latest-git'
 maintainer 'Alexander Pyatkin'
 maintainer_email 'aspyatkin@gmail.com'
 license 'MIT'
-version '1.3.0'
+version '1.4.0'
 description 'Installs and configures git'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 
 recipe 'latest-git', 'Installs and configures git'
-depends 'git', '~> 6.1.0'
+depends 'git', '~> 8.0.0'
 depends 'apt'
 
 source_url 'https://github.com/aspyatkin/latest-git-cookbook'
