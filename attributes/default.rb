102e1485bd10f61f851d0df567ba56ac55f165ba
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2018-04-21 18:49:24 +0400

    Remove git cookbook strict version dependency

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
 id = 'latest-git'
 
-default[id]['source']['version'] = '2.14.1'
+default[id]['source']['version'] = '2.17.0'
 default[id]['source']['url_template'] = \
   'https://www.kernel.org/pub/software/scm/git/git-%{version}.tar.gz'
 default[id]['source']['checksum'] = \
-  '01925349b9683940e53a621ee48dd9d9ac3f9e59c079806b58321c2cf85a4464'
+  '7a0cff35dbb14b77dca6924c33ac9fe510b9de35d5267172490af548ec5ee1b8'
